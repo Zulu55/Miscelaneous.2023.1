1645d4a089a498b68397ba0d1ee1526e9bdd80b2
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-05-02 19:33:38 -0500

    Moda implementation

--- Miscelaneous/Program.cs
+++ Miscelaneous/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Miscelaneous;
 
+Console.WriteLine("UTILITIES");
+var numbers = Utilities.Fill(1000, 0, 99);
+Console.WriteLine(Utilities.ToString(numbers));
+Console.WriteLine("MODA");
+var modas = Utilities.GetModas(numbers);
+Console.WriteLine(Utilities.ToString(modas));
---
+++ Miscelaneous/Utilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace Miscelaneous
+{
+    public static class Utilities
+	{
+        public static List<int> GetModas(List<int> numbers)
+        {
+            var counters = new Dictionary<int, int>();
+            foreach (var number in numbers)
+            {
+                if (counters.ContainsKey(number))
+                {
+                    counters[number]++; 
+                }
+                else
+                {
+                    counters.Add(number, 1);
+                }
+            }
+            var maxValue = counters.Max(x => x.Value);
+            return counters
+                .Where(x => x.Value == maxValue)
+                .Select(x => x.Key)
+                .ToList();
+        }
+
+        public static List<int> Fill(int n, int min, int max)
+        {
+            var random = new Random();
+            var numbers = new List<int>();
+            max++;
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(random.Next(min, max));
+            }
+            return numbers;
+        }
+
+        public static string ToString(List<int> numbers)
+        {
+            var output = string.Empty;
+            foreach (var number in numbers)
+            {
+                output += $"{number}\t";
+            }
+            return output + "\n";
+        }
+    }
+}
+
