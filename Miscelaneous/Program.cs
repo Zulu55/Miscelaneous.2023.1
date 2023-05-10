377f640c0cbd0be87f188b89d80960eed7b22513
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-05-09 19:14:17 -0500

    Anagrams solved

--- Miscelaneous/Program.cs
+++ Miscelaneous/Program.cs
@@ -1,8 +1,30 @@
 ﻿using Miscelaneous;
 
 Console.WriteLine("UTILITIES");
-var numbers = Utilities.Fill(1000, 0, 99);
-Console.WriteLine(Utilities.ToString(numbers));
-Console.WriteLine("MODA");
-var modas = Utilities.GetModas(numbers);
-Console.WriteLine(Utilities.ToString(modas));
+var changes = Utilities.Anagrams(
+    new List<string>
+    {
+        "Los",
+        "Perro",
+        "Pan",
+        "La vida es bella",
+        "saroihdsrgpoigreu89er5wuyaesfoipwy4tyhdfgsalkjrsthjgdfasviouyergoihvdasiodfshñoasdifywoer9yhgadfrsjklñhdsbfljkys"
+    },
+    new List<string>
+    {
+        "Sol",
+        "Gato",
+        "Paz",
+        "Bella es tu boca",
+        "dpioujvdfkjdhrsgidsfhasrfgyiughjkdfgrliergiÁopaegrpegru´90we5tñvdfjlisrteuyp9gdfuadsofyvdsalkjhergwoiuyewsefiode"
+    });
+foreach (var change in changes)
+{
+    Console.WriteLine(change);
+}
+
+//var numbers = Utilities.Fill(1000, 0, 99);
+//Console.WriteLine(Utilities.ToString(numbers));
+//Console.WriteLine("MODA");
+//var modas = Utilities.GetModas(numbers);
+//Console.WriteLine(Utilities.ToString(modas));
--- Miscelaneous/Utilities.cs
+++ Miscelaneous/Utilities.cs
@@ -5,6 +5,83 @@ namespace Miscelaneous
 {
     public static class Utilities
 	{
+        public static List<int> Anagrams(List<string> sentencesSource, List<string> sentencesTarget)
+        {
+            if (sentencesSource.Count != sentencesTarget.Count)
+            {
+                throw new Exception("The lists have different elements number.");
+            }
+            var output = new List<int>();
+            for (int i = 0; i < sentencesSource.Count; i++)
+            {
+                output.Add(Anagrams(sentencesSource[i], sentencesTarget[i]));
+            }
+            return output;
+        }
+
+        public static int Anagrams(string source, string target)
+        {
+            if (source.Length != target.Length)
+            {
+                return -1;
+            }
+
+            var sourceDictionary = GetDictionary(source);
+            var targetDictionary = GetDictionary(target);
+
+            var counter = 0;
+            var i = 0;
+            var j = 0;
+            while (i < sourceDictionary.Count && j < targetDictionary.Count)
+            {
+                if (sourceDictionary.ElementAt(i).Key == targetDictionary.ElementAt(j).Key)
+                {
+                    counter += Math.Abs(sourceDictionary.ElementAt(i).Value - targetDictionary.ElementAt(j).Value);
+                    i++;
+                    j++;
+                }
+                else if (sourceDictionary.ElementAt(i).Key < targetDictionary.ElementAt(j).Key)
+                {
+                    counter += sourceDictionary.ElementAt(i).Value;
+                    i++;
+                }
+                else
+                {
+                    counter += targetDictionary.ElementAt(j).Value;
+                    j++;
+                }
+            }
+            while (i < sourceDictionary.Count)
+            {
+                counter += sourceDictionary.ElementAt(i).Value;
+                i++;
+            }
+            while (j < targetDictionary.Count)
+            {
+                counter += targetDictionary.ElementAt(j).Value;
+                j++;
+            }
+
+            return counter / 2;
+        }
+
+        private static SortedDictionary<char, int> GetDictionary(string sentence)
+        {
+            var dictionary = new SortedDictionary<char, int>();
+            foreach (var character in sentence.ToLower())
+            {
+                if (dictionary.ContainsKey(character))
+                {
+                    dictionary[character]++;
+                }
+                else
+                {
+                    dictionary.Add(character, 1);
+                }
+            }
+            return dictionary;
+        }
+
         public static List<int> GetModas(List<int> numbers)
         {
             var counters = new Dictionary<int, int>();
